a07ff81c443d067f498386d3ddebc8d516923935
davermn5 <davermn5@gmail.com>
2026-02-06 15:52:19 -0800

    Added code for updating a Box (heap) via a mutable reference (stack).

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello");
+
+    /* Updating a Box (heap) via a mutable reference (stack)
+    let mut b: Box<i32> = Box::new(1);
+    let c: &mut Box<i32> = &mut b;
+
+    *c = Box::new(2); // OR **c = 2;
+
+    println!("{c}");
+     */
 }
