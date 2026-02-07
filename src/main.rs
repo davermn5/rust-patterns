8f384376b9a172505189ed294800d1ab8234d68a
davermn5 <davermn5@gmail.com>
2026-02-06 16:08:44 -0800

    Updating a Box(heap) via a several-layer deep (stacked) mutable references.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 fn main() {
 
+    /* Updating a Box (heap) via a mutable reference (stack) several mutable refs deep..
+    let mut a: Box<i32> = Box::new(1);
+
+    let mut b: &mut Box<i32> = &mut a;
+    let mut c: &mut &mut Box<i32> = &mut b;
+    let mut d: &mut &mut &mut Box<i32> = &mut c;
+    let mut e: &mut &mut &mut &mut Box<i32> = &mut d;
+    let mut f: &mut &mut &mut &mut &mut Box<i32> = &mut e;
+    let mut g: &mut &mut &mut &mut &mut &mut Box<i32> = &mut f;
+    let mut h: &mut &mut &mut &mut &mut &mut &mut Box<i32> = &mut g;
+    let i: &mut &mut &mut &mut &mut &mut &mut &mut Box<i32> = &mut h;
+
+    ********i = Box::new(2);   //OR *********i = 2;
+
+    println!("{i}");
+    */
+
+
     /* Updating a Box (heap) via a mutable reference (stack)
     let mut b: Box<i32> = Box::new(1);
     let c: &mut Box<i32> = &mut b;
