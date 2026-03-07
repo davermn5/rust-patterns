90266efd690a603b40409f2c62003b1124224f1b
davermn5 <davermn5@gmail.com>
2026-03-06 18:03:18 -0800

    Minor tweak to function body; Now uses a backreference (*) for accessing the actual value needed from the functions referenced integer.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 //  a pair of numbers (i.e. "Addends") that sum to target, false otherwise. (This problem is similar to TwoSum; However in Two Sum, the input
 //  is not sorted.)
 //  Time Complexity of Solution = Linear O(n)
-fn sorted_twosums(data: &[i32], target: i32) -> bool {
+fn sorted_twosums(data: &[i32], target: &i32) -> bool {
     if data.len() < 2 { return false; }
 
     let mut x = 0;
@@ -12,12 +12,12 @@ fn sorted_twosums(data: &[i32], target: i32) -> bool {
 
     while x < y {
 
-        let sum = &data[x] + &data[y];
+        let sum = data[x] + data[y];
 
-        if sum > target {
+        if sum > *target {
             y -= 1;
         }
-        else if sum < target {
+        else if sum < *target {
             x += 1;
         }
         else {
@@ -29,6 +29,6 @@ fn sorted_twosums(data: &[i32], target: i32) -> bool {
 
 fn main() {
     let arr_i32: [i32; 6] = [0,1,4,6,9,10];
-    let has_two_addends: bool = sorted_twosums(&arr_i32, 5);
+    let has_two_addends: bool = sorted_twosums(&arr_i32, &5);
     println!("Has two addends? {}", has_two_addends);
 }
